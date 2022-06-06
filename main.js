9aeab0ceb81cd751c9dd98c1ccd4ab905f8e0711
Li Hui <lihuicham@gmail.com>
2022-06-06 15:59:23 +0800

    Syncing with date

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-// get current time 
+// Get current date 
 
 function clock() {
     const today = new Date();
-    
+
+    //---------TIME----------
+
     let hours = today.getHours();
     let minutes = today.getMinutes();
     let seconds = today.getSeconds();
@@ -19,12 +21,29 @@ function clock() {
     hours = hours < 10 ? "0" + hours : hours
     minutes = minutes < 10 ? "0" + minutes : minutes
     seconds = seconds < 10 ? "0" + seconds : seconds
-    
+
     //change the html content 
     document.querySelector(".hours").innerHTML = hours;
     document.querySelector(".minutes").innerHTML = minutes;
     document.querySelector(".seconds").innerHTML = seconds;
     document.querySelector(".period").innerHTML = period;
+
+    //---------DATE----------
+
+    let dayNumber = today.getDate();
+    const dayName = today.toLocaleString("default", {weekday: "long"});
+    const monthName = today.toLocaleString("default", {month: "short"});
+    const year = today.getFullYear();
+
+    // Add the 0 for day number lower than 10 
+
+    dayNumber = dayNumber < 10 ? "0" + dayNumber : dayNumber;
+
+    document.querySelector(".day_number").innerHTML = dayNumber;
+    document.querySelector(".day_name").innerHTML = dayName;
+    document.querySelector(".month_name").innerHTML = monthName;
+    document.querySelector(".year").innerHTML = year;
+
 }
 
 setInterval(clock, 1000)
